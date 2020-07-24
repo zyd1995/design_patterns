6014dbbda82233bf82e25b3595d31226d447c151
张一丹 <zhangyidan@zhangyidandeMacBook-Pro.local>
2020-07-24 15:09:21 +0800

    update

--- singleton_test.cpp
+++ singleton_test.cpp
@@ -4,17 +4,19 @@
 using namespace std;
 
 class singleton{
-    static singleton instance;  //静态成员变量
-    singleton(){};              //私有构造函数，不允许类外调用new构造实例
+    static singleton* instance;  //静态成员变量,该类唯一实例
+    singleton(){                //私有构造函数，不允许类外调用new构造实例
+        cout<<"constructor called!"<<endl; 
+        }              
 public:
-    static singleton getinstance(){   //只能通过调用类的getinstance函数构造实例
-        if(instance==NULL)           //当实例未构造时才能构造实例
+    static singleton* getinstance(){   //只能通过调用类的getinstance函数构造实例
+        if(instance==nullptr)           //当实例未构造时才能构造实例
             instance=new singleton();  
         return instance;
     }
 };
 
 int main(){
-    singleton s=singleton::getinstance();
-    singleton s1=singleton::getinstance();//s,s1是相同的实例 s==s1为true
+    singleton* s=singleton::getinstance();
+    singleton* s1=singleton::getinstance();//s,s1是相同的实例 s==s1为true
 }
